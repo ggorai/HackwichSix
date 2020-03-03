049b664a1aab089bead9bdcbe50d853de70040c4
CM Student <cmstudent@uhwo-acm-c-14.local>
2020-03-03 10:50:17 -1000

    HackwichSix

--- HackwichSix/ViewController.swift
+++ HackwichSix/ViewController.swift
@@ -8,8 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+    
+     //array to display in the table view
+    var myCountriesarray=[ "japan" , "germany", "greece"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return myCountriesarray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")!
+        let text = myCountriesarray[indexPath.row]
+        cell.textLabel?.text = text
+        return cell
 
+        
+    }
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
