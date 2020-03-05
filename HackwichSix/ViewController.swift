54dd484b5dde862264f406b7dc141532310436a0
CM Student <cmstudent@uhwo-acm-c-14.local>
2020-03-05 10:40:59 -1000

    Hackwich 6 (Problem Set)

--- HackwichSix/ViewController.swift
+++ HackwichSix/ViewController.swift
@@ -13,16 +13,18 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
      //array to display in the table view
     var myCountriesarray=[ "japan" , "germany", "greece"]
     
+    var myCountrieyearsarray = [ "2020" , " 2021" , "2022"]
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
         return myCountriesarray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")!
         let text = myCountriesarray[indexPath.row]
         cell.textLabel?.text = text
+        cell.detailTextLabel?.text = myCountrieyearsarray[indexPath.row]
         return cell
 
         
